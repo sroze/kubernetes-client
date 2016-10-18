3cfc518a7c3706a2180e16522e6ad5de75d59f48
Samuel ROZE <samuel.roze@gmail.com>
2016-10-18 17:02:32 +0100

    Allow to find all the deployments

---
+++ src/Model/DeploymentList.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Kubernetes\Client\Model;
+
+class DeploymentList implements \IteratorAggregate
+{
+    /**
+     * @var Deployment[]
+     */
+    private $items = [];
+
+    /**
+     * @param Deployment[] $deployments
+     *
+     * @return DeploymentList
+     */
+    public static function fromDeployments(array $deployments)
+    {
+        $list = new self();
+        $list->items = $deployments;
+
+        return $list;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->items);
+    }
+
+    /**
+     * @return Deployment[]
+     */
+    public function getDeployments()
+    {
+        return $this->items;
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->items);
+    }
+}
